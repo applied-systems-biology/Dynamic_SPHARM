55874aaf643907dda1fce640d9131e621cfaa660
Anna Medyukhina <anna.medyukhina@hki-jena.de>
2019-09-04 17:20:45 +0200

    scale factor as parameter

--- SPHARM/classes/surface.py
+++ SPHARM/classes/surface.py
@@ -304,7 +304,7 @@ class Surface(object):
 
         return grid
 
-    def plot_points(self):
+    def plot_points(self, scale_factor=0.1):
         """
         Plot a 3D view of the surface points with mayavi.
         
@@ -316,7 +316,7 @@ class Surface(object):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             mesh = mlab.points3d(self.x, self.y, self.z, self.z, scale_mode='none',
-                                 scale_factor=0.1, mode='sphere', colormap='gray').scene
+                                 scale_factor=scale_factor, mode='sphere', colormap='gray').scene
             mesh.background = (1, 1, 1)
             mesh.magnification = 3
         return mesh
